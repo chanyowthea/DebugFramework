8175fd384c633a2f6e341b76e043961ac754bdf2
unknown <wei.xu@corp.jingle.cn>
2018-09-11 15:01:34 +0800

    up

---
+++ Debugger.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using UnityEngine.Internal;
+
+namespace DebugFramework
+{
+    public enum LogColor
+    {
+        None,
+        Green,
+        Yellow,
+        Red
+    }
+
+    public class Debugger
+    {
+        public static bool EnableLog = true;
+
+        public static bool LogToFile = true;
+
+        public static void Log(object message)
+        {
+            Log(message, null);
+        }
+
+        public static void Flush()
+        {
+            if (LogToFile && DebuggerFileOutput.instance != null)
+            {
+                DebuggerFileOutput.instance.FlushToFile();
+            }
+        }
+        public static void OnApplicationQuit()
+        {
+            if (LogToFile && DebuggerFileOutput.instance != null)
+            {
+                DebuggerFileOutput.instance.FlushToFile();
+                DebuggerFileOutput.instance.Close();
+            }
+        }
+
+        public static void Log(object message, LogColor type)
+        {
+            if (EnableLog)
+            {
+                string fmtMessage = FormatMessage(message);
+                WriteToFile(fmtMessage);
+                string format;
+                switch (type)
+                {
+                    case LogColor.Green:
+                        format = "<color=green>{0}</color>";
+                        break;
+                    case LogColor.Yellow:
+                        format = "<color=yellow>{0}</color>";
+                        break;
+                    case LogColor.Red:
+                        format = "<color=red>{0}</color>";
+                        break;
+                    default:
+                        format = "{0}";
+                        break;
+                }
+                Debug.Log(string.Format(format, fmtMessage));
+            }
+        }
+
+        public static void Log(object message, UnityEngine.Object context)
+        {
+            if (EnableLog)
+            {
+                string fmtMessage = FormatMessage(message);
+                WriteToFile(fmtMessage);
+                Debug.Log(fmtMessage, context);
+            }
+        }
+        private static string FormatMessage(object message)
+        {
+            return string.Format("[{0}] {1}", System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), message);
+        }
+
+        public static void LogError(object message)
+        {
+            LogError(message, null);
+        }
+
+        public static void LogError(object message, Object context)
+        {
+            if (EnableLog)
+            {
+                string fmtMessage = FormatMessage(message);
+                WriteToFile(fmtMessage);
+                Debug.LogError(fmtMessage, context);
+            }
+        }
+
+        public static void LogWarning(object message)
+        {
+            LogWarning(message, null);
+        }
+
+        public static void LogWarning(object message, Object context)
+        {
+            if (EnableLog)
+            {
+                string fmtMessage = FormatMessage(message);
+                WriteToFile(fmtMessage);
+                Debug.LogWarning(fmtMessage, context);
+            }
+        }
+
+        public static void DrawLine(Vector3 start, Vector3 end, Color color)
+        {
+            if (EnableLog)
+            {
+                Debug.DrawLine(start, end, color);
+            }
+        }
+
+        public static void DrawLine(Vector3 start, Vector3 end, Color color, float duration)
+        {
+            if (EnableLog)
+            {
+                Debug.DrawLine(start, end, color, duration);
+            }
+        }
+
+        public static void DrawLine(Vector3 start, Vector3 end)
+        {
+            if (EnableLog)
+            {
+                Debug.DrawLine(start, end);
+            }
+        }
+
+        public static void DrawLine(Vector3 start, Vector3 end, [DefaultValue("Color.white")] Color color, [DefaultValue("0.0f")] float duration, [DefaultValue("true")] bool depthTest)
+        {
+            if (EnableLog)
+            {
+                Debug.DrawLine(start, end, color, duration, depthTest);
+            }
+        }
+        public static void WriteToFile(string message)
+        {
+            if (LogToFile && DebuggerFileOutput.instance != null)
+            {
+                DebuggerFileOutput.instance.Log(message);
+            }
+        }
+    }
+}
---
+++ DebuggerFileOutput.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace DebugFramework
+{
+    public class DebuggerFileOutput : MonoBehaviour
+    {
+        static public DebuggerFileOutput instance = null;
+
+        private List<string> m_OutputText = new List<string>();
+
+        private string m_OutputFilePath;
+        private StreamWriter m_Writer;
+        void Start()
+        {
+            m_OutputFilePath =
+#if UNITY_EDITOR || UNITY_STANDALONE
+            Application.dataPath
+#else
+            Application.persistentDataPath
+#endif
+            + "/debugger.log";
+            m_Writer = new StreamWriter(m_OutputFilePath, false, Encoding.UTF8);
+            m_Writer.AutoFlush = true;
+            //ready
+            instance = this;
+        }
+        void Update()
+        {
+            if (m_Writer == null)
+            {
+                return;
+            }
+            Flush();
+        }
+        void OnDestroy()
+        {
+            if (m_Writer != null)
+            {
+                m_Writer.Flush();
+                Close();
+            }
+        }
+        public void Log(string msg)
+        {
+            lock (m_OutputText)
+            {
+                m_OutputText.Add(msg);
+            }
+        }
+        public void Flush()
+        {
+            if (m_Writer == null)
+            {
+                return;
+            }
+            lock (m_OutputText)
+            {
+                if (m_OutputText.Count > 0)
+                {
+                    foreach (string t in m_OutputText)
+                    {
+                        m_Writer.WriteLine(t);
+                    }
+                    m_OutputText.Clear();
+                }
+            }
+        }
+
+        public void FlushToFile()
+        {
+            Flush();
+            if (m_Writer != null)
+            {
+                m_Writer.Flush();
+            }
+        }
+        public void Close()
+        {
+            if (m_Writer != null)
+            {
+                m_Writer.Close();
+                m_Writer = null;
+            }
+        }
+    }
+}
